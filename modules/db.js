0eb2cfd6145e6ed3212819954b43336c5a45323d
Neota Moe <neota.moe@gmail.com>
2017-05-14 20:47:40 -0500

    configured server for heroku/mongo deploy

--- modules/db.js
+++ modules/db.js
@@ -1,7 +1,9 @@
 // requires
 var mongoose = require ('mongoose');
 
-var mongoDB = mongoose.connect('mongodb://localhost:27017/faveflicks').connection;
+// var mongoDB = mongoose.connect('mongodb://localhost:27017/faveflicks').connection;
+var mongoDB = mongoose.connect('mongodb://heroku_lq2rhd4r:d0842t40gdi95sjjpfnvhsqgn0@ds139761.mlab.com:39761/heroku_lq2rhd4r').connection;
+
 
 mongoDB.on ('error', function(err){
   console.log('mongoDB error:', err);
